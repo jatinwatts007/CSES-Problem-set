a37c849f9ab22bd62b8f3b0241c4c503728434e4
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2022-03-03 22:30:26 +0530

    Create trailing zeros.cpp

---
+++ trailing zeros.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+   long long n;
+   cin>>n;
+   long long ans=0;
+   for(long long i=5;n/i>=1;i*=5){
+       ans+=(n/i);
+   }
+   
+   cout<<ans;
+   return 0;
+}
